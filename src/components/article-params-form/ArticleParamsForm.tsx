96e683b05c01f3e9a05776d86992cdc2d91180c8
kerzolys <kerzolys@gmail.com>
2024-08-21 18:27:48 +0300

    проектная работа выполнена

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,38 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { ReactNode, useRef, useState } from 'react';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+export type TArticleParamsForm = {
+	children: ReactNode
+	onSubmit: () => void
+	onReset: () => void
+}
+
+export const ArticleParamsForm = ({ children, onSubmit, onReset }: TArticleParamsForm) => {
+	const [open, setOpen] = useState(false)
+	const openForm = () => {
+		setOpen(prev => !prev)
+	}
+
+	const formRef = useRef<HTMLFormElement | null>(null)
+
+	const handleSubmit = (evt: React.FormEvent) => {
+		evt.preventDefault()
+		onSubmit()
+	}
+	const handleReset = () => {
+		onReset()
+		formRef.current?.reset()
+	}
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={openForm} isOpen={open} />
+			<aside className={clsx(styles.container, { [styles.container_open]: open, [styles.container]: !open })} >
+				<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset} ref={formRef}>
+					{children}
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
